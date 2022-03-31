b762f76fbed64e6a7be714f119c562bb5ca0a8af
khaifazil <khairal.fazil@gmail.com>
2022-03-31 16:43:44 +0800

    completed activity

--- calculator.go
+++ calculator.go
@@ -4,23 +4,29 @@ import "fmt"
 
 func add(a, b int) int {
 	//Insert code here
-
+	result := a + b
+	return result
 }
 
 func subtract(a, b int) int {
 	//Insert code here
-
+	result := a - b
+	return result
 }
 
 func multiply(a, b int) int {
 	//Insert code here
-
+	result := a * b
+	return result
 }
 
-func divide(a, b int) int {
+func divide(a, b int) float32 {
 	//Insert code here
 	//consider for b = 0
-
+	var x float32 = float32(a)
+	var y float32 = float32(b)
+	result := x / y
+	return result
 }
 
 func main() {
@@ -34,4 +40,21 @@ func main() {
 	fmt.Scanln(&b)
 
 	//Insert code here
+	if a == 0 || b == 0 {
+		fmt.Println("Cannot divide by 0")
+	}else if process == "add" {
+		ans := add(a, b)
+		fmt.Println("The answer is :", ans)
+	}else if process == "sub" {
+		ans := subtract(a, b)
+		fmt.Println("The answer is :", ans)
+	}else if process == "mul" {
+		ans := multiply(a, b)
+		fmt.Println("The answer is :", ans)
+	}else if process == "div" {
+		ans := divide(a, b)
+		fmt.Println("The answer is :", ans)
+	}
+
+	main()
 }
